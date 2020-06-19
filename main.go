30cb84adb1b9f1f08e06d6cfda424e8e0449c238
xiaoboya <xiaoboya@owntek.com>
2020-06-19 17:51:10 +0800

    add:添加方法（获取文件夹结构）

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 /*
  * @Author: xiaoboya
  * @Date: 2020-06-19 16:15:26
- * @LastEditTime: 2020-06-19 16:15:39
+ * @LastEditTime: 2020-06-19 17:32:34
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /MyDiskServer/main.go
  */
 
 package main
+
+func main() {
+
+}
