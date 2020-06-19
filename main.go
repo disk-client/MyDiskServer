30598883e6cf2b34c43298a5c4cf6277617248f2
xiaoboya <xiaoboya@owntek.com>
2020-06-19 17:14:38 +0800

    add:上传文件

---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+ * @Author: xiaoboya
+ * @Date: 2020-06-19 16:15:26
+ * @LastEditTime: 2020-06-19 16:15:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /MyDiskServer/main.go
+ */
+
+package main
